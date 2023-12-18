4e75937fa69c107d763f5ed2aacc92f0a3d4a0bd
yandex-lyceum-yakovlev <yakovlev@yandexlyceum.ru>
2023-12-18 17:19:35 +0400

    1

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-print("Hello World")
+import tkinter as tk
+
+window = tk.Tk()
+canvas = tk.Canvas(window, bg='blue', height=600, width=600)
+canvas.create_line((0, 0), (600, 600), fill='red')
+canvas.pack()
+window.mainloop()
 
