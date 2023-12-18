04e9a9e456466fb47dd9e94049b66f9b07815cf2
yandex-lyceum-yakovlev <yakovlev@yandexlyceum.ru>
2023-12-18 18:54:08 +0400

    повторить

---
+++ test.py
@@ -0,0 +1,39 @@
+# # ввод до пустой строки
+# a = []
+# s = input()
+# while s:
+#     a.append(int(s))
+#     s = input()
+# print(a)
+
+# # ввод заданного числа строк
+# a = []
+# n = int(input())
+# for i in range(n):
+#     a.append(int(input()))
+# print(a)
+
+# # ввод нескольких чисел на одной строке через разделитель ", "
+# a = [int(i) for i in input().split(", ")]
+# print(a)
+
+# вывод
+# a = [1, 2, 3]
+# print(*a, sep=", ")
+# s = ":".join([str(i) for i in a])
+# print(s)
+
+# посчет
+# d = dict()
+# s = ("sretfugvwlieturhgiwrhtgiuhwrt")
+# for i in s:
+#     d[i] = d.get(i, 0) + 1
+# print(d)
+# print(d.values())
+
+# list str
+# sum len sort sorted
+# lower upper capitalize isalpha islower...
+
+# set
+# порядок не важен, без повторений ...
