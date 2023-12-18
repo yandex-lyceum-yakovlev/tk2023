f6e8236a336dbcd0203331fda3069014cfa8f6c4
yandex-lyceum-yakovlev <yakovlev@yandexlyceum.ru>
2023-12-18 18:54:40 +0400

    повторить

--- main.py
+++ main.py
@@ -1,11 +1,39 @@
-import tkinter
+# # ввод до пустой строки
+# a = []
+# s = input()
+# while s:
+#     a.append(int(s))
+#     s = input()
+# print(a)
 
-def draw(event):
-    canvas.create_oval((100, 100), (300, 300), fill='red')
+# # ввод заданного числа строк
+# a = []
+# n = int(input())
+# for i in range(n):
+#     a.append(int(input()))
+# print(a)
 
+# # ввод нескольких чисел на одной строке через разделитель ", "
+# a = [int(i) for i in input().split(", ")]
+# print(a)
 
-master = tkinter.Tk()
-canvas = tkinter.Canvas(master, bg='blue', height=600, width=600)
-canvas.pack()
-master.bind("<KeyPress>", draw)
-master.mainloop()
+# вывод
+# a = [1, 2, 3]
+# print(*a, sep=", ")
+# s = ":".join([str(i) for i in a])
+# print(s)
+
+# посчет
+# d = dict()
+# s = ("sretfugvwlieturhgiwrhtgiuhwrt")
+# for i in s:
+#     d[i] = d.get(i, 0) + 1
+# print(d)
+# print(d.values())
+
+# list str
+# sum len sort sorted
+# lower upper capitalize isalpha islower...
+
+# set
+# порядок не важен, без повторений ...
